7749f674099e5443c89d522396f03e87a5ca4c6c
PeicoPaco <rodrigo.reategui.g@gmail.com>
2022-11-06 22:02:11 -0500

    first try for background

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -4,10 +4,17 @@ import './Home.css'
 function Home() {
   return (
     <>
-    <div id='left-top-background-home'>LeftTop</div>
-    <div id='right-top-background-home'>RightTop</div>
-    <div id='left-bottom-background-home'>LeftBottom</div>
-    <div id='right-bottom-background-home'>RigthBottom</div>
+    <div id='left-top-background-home'></div>
+    <div id='right-top-background-home'></div>
+    <div id='left-bottom-background-home'></div>
+    <div id='right-bottom-background-home'></div>
+    <div className='navbar-home'></div>
+        <ul className='navbar-box-content-home'>
+            <li className='navbar-link-home'>¿Quiénes somos?</li>
+            <li className='navbar-link-home'>Servicios</li>
+            <li className='navbar-link-home'>Testimonios</li>
+            <li className='navbar-link-home'>PDU</li>
+        </ul>
     </>
   )
 }
