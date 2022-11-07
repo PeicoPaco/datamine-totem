155630b95832fd4f3a6cc25417f1aa99ab041135
PeicoPaco <rodrigo.reategui.g@gmail.com>
2022-11-07 00:46:36 -0500

    first half of home

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -4,17 +4,28 @@ import './Home.css'
 function Home() {
   return (
     <>
-    <div id='left-top-background-home'></div>
-    <div id='right-top-background-home'></div>
-    <div id='left-bottom-background-home'></div>
-    <div id='right-bottom-background-home'></div>
-    <div className='navbar-home'></div>
-        <ul className='navbar-box-content-home'>
-            <li className='navbar-link-home'>¿Quiénes somos?</li>
-            <li className='navbar-link-home'>Servicios</li>
-            <li className='navbar-link-home'>Testimonios</li>
-            <li className='navbar-link-home'>PDU</li>
-        </ul>
+        <div id='left-top-background-home'></div>
+        <div id='right-top-background-home'></div>
+        <div id='left-bottom-background-home'></div>
+        <div id='right-bottom-background-home'></div>
+        <div className='navbar-home'>
+            <ul className='navbar-box-content-home'>
+                <li className='navbar-link-home'>¿Quiénes somos?</li>
+                <li className='navbar-link-home'>Servicios</li>
+                <li className='navbar-link-home'>Testimonios</li>
+                <li className='navbar-link-home'>PDU</li>
+            </ul>
+        </div>
+        <div className='center-image-home'></div>
+        <div className='title-box-home'>
+            <h1 className='title-home'>¿Quiénes somos?</h1>
+        </div>
+        <div className='description-box-home'>
+            <p>Somos el principal proveedor mundial de tecnologóa para planificación y 
+                gestión de las operaciones mineras. Brindamos soluciones que abarcan 
+                las etapas de exploración, modelamienti, planificación minera, operaciones, 
+                logística, comercialización y gestión de ESG</p>
+        </div>
     </>
   )
 }
